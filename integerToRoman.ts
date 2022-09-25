2b787101eecac970e3e96c12cb923400bcf33f31
BrenoMorim <brenomorim13@gmail.com>
2022-09-25 11:49:17 -0300

    Refatorando o código do desafio integerToRoman

--- integerToRoman.ts
+++ integerToRoman.ts
@@ -1,63 +1,45 @@
 /**
  * Dado um número inteiro, a função retorna um número romano equivalente, através
- * de um loop while que a cada ciclo vai subtraindo o número inteiro enquanto constrói o romano
+ * de um forEach de um array com os valores de cada número romano em inteiro,
+ * usando a lógica de divisão inteira para descobrir quantos algarismos romanos
+ * devem ser adicionados e quanto deve ser subtraído do número inicial
  */
 /**
  * Given an integer number, the function turns it into a roman number
  */
 function intToRoman(num: number): string {
     let roman = '';
-    while (num > 0) {
-        if (num >= 1000) {
-            roman += 'M';
-            num -= 1000;
-            continue;
-        }
-        if (num >= 500) {
-            roman += 'D';
-            num -= 500;
-            continue;
-        }
-        if (num >= 100) {
-            roman += 'C';
-            num -= 100;
-            continue;
-        }
-        if (num >= 50) {
-            roman += 'L';
-            num -= 50;
-            continue;
-        }
-        if (num >= 10) {
-            roman += 'X';
-            num -= 10;
-            continue;
-        }
-        if (num >= 5) {
-            roman += 'V';
-            num -= 5;
-            continue;
-        }
-        if (num >= 1) {
-            roman += 'I';
-            num -= 1;
-            continue;
-        }
-    }
+
+    romanValues.forEach(n => {
+        const division = Math.floor(num / n.value);
+        num -= division * n.value;
+        roman += n.roman.repeat(division);
+    });
+
     // Special cases:
-    // 400
-    roman = roman.replace('CCCC', 'CD');
-    // 900
-    roman = roman.replace('DCD', 'CM');
-    // 90
-    roman = roman.replace('LXXXX', 'XC');
-    // 40
-    roman = roman.replace('XXXX', 'XL');
-    // 9
-    roman = roman.replace('VIIII', 'IX');
-    // 4
-    roman = roman.replace('IIII', 'IV');
+
+    roman = roman.replace('CCCC', 'CD'); // 400
+    roman = roman.replace('DCD', 'CM'); // 900
+    roman = roman.replace('LXXXX', 'XC'); // 90
+    roman = roman.replace('XXXX', 'XL'); // 40
+    roman = roman.replace('VIIII', 'IX'); // 9
+    roman = roman.replace('IIII', 'IV'); // 4
+
     return roman;
 };
 
-console.log(intToRoman(1994));
+const romanValues = [
+    {roman: 'M', value: 1000},
+    {roman: 'D', value: 500},
+    {roman: 'C', value: 100},
+    {roman: 'L', value: 50},
+    {roman: 'X', value: 10},
+    {roman: 'V', value: 5},
+    {roman: 'I', value: 1},
+];
+
+// Tests
+
+console.log(intToRoman(1994)); // MCMXCIV
+console.log(intToRoman(19)); // XIX
+console.log(intToRoman(36)); // XXXVI
